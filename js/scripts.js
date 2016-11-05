61f5fc602eef6a2146d0ea6c8d7a2bec74e2dd59
Sara Jensen <jensen.sara.e@gmail.com>
2016-11-05 14:21:21 -0700

    finish backend logic and front end display if the weather is partly cloudy or clear skies the app returns outdoor activities, other weather returns indoor activities.

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,24 @@ function Weather() {
 }
 
 Weather.prototype.getWeather = function(zipcode) {
-  $.get('http://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&appid=4114b39093003af2a4520831ad994aa7').then(function(response) {
+  $.get('http://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&appid=4114b39093003af2a4520831ad994aa7')
+  .then(function(response) {
     $('#weather').text("You'll see " + response.weather[0].description + " in " + response.name + " today.");
-    $('#outfit').text("blah");
+    if (response.weather[0].description === "clear sky" || "scattered clouds") {
+      $('#first-title').text("Running");
+      $('#first-body').text("Run like the wind! It's a beautiful day.");
+      $('#second-title').text("Bike");
+      $('#second-body').text("Pedal to the metal!");
+      $('#third-title').text("Tennis");
+      $('#third-body').text("Meet up with a friend at your local court!");
+    } else {
+      $('#first-title').text("Dancing");
+      $('#first-body').text("Dance like no one is watching! Or try a local dance class.");
+      $('#second-title').text("Yoga");
+      $('#second-body').text("Oooommmm");
+      $('#third-title').text("Swimming");
+      $('#third-body').text("Laps at your local gym. Try for 500 meters!");
+    }
   });
 };
 
